a51b1217195ad382fd8e7139b3d3c6137a2c6e65
Built_by_Ali <alizahid2710@gmail.com>
2026-09-06 14:37:07 -0700

    Change CartoDB tile layer to free no-key mode
    Updated tile layer URL for CartoDB Dark Matter tiles to a free mode without API key.

--- app.js
+++ app.js
@@ -915,12 +915,12 @@ function initMap() {
     attributionControl: true
   });
 
-  // CartoDB Dark Matter Futuristic Tiles
-  L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+// CartoDB Dark Matter Futuristic Tiles (Free No-Key Mode)
+L.tileLayer('https://a.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
     subdomains: 'abcd',
     maxZoom: 19
-  }).addTo(AppState.map);
+}).addTo(AppState.map);
 
   // Click on map to inspect weather at coordinates
   AppState.map.on('click', async (e) => {
