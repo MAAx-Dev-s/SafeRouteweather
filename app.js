ce2b05efa63e428170467856fae68557e16af917
Built_by_Ali <alizahid2710@gmail.com>
2026-09-06 14:43:54 -0700

    Replace CartoDB tiles with Stadia Alidade tiles

--- app.js
+++ app.js
@@ -915,10 +915,9 @@ function initMap() {
     attributionControl: true
   });
 
-// CartoDB Dark Matter Futuristic Tiles (Free No-Key Mode)
-L.tileLayer('https://a.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-    subdomains: 'abcd',
+// Stadia Alidade Smooth Dark Tiles (Free Open Alternative)
+L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
+    attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>',
     maxZoom: 19
 }).addTo(AppState.map);
 
